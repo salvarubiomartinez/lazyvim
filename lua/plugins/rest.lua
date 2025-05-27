657955458309f1edbc0304714bf8620a57da297b
rubio <salvarubiomartinez@gmail.com>
2025-05-27 10:06:27 +0200

    debugger added

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,6 +1,16 @@
 return {
   {
     "rest-nvim/rest.nvim",
+    config = function()
+      require("rest-nvim").setup({
+        request = {
+          skip_ssl_verification = true, -- Set to true to skip SSL verification
+        },
+        -- Add any other rest.nvim specific options here
+        -- For example:
+        -- result_split_horizontal = false,
+      })
+    end,
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       opts = function(_, opts)
